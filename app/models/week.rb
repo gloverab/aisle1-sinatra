8a1924ff8c4285b2defa7afdd92bb8d12d621bf1
Alex Glover <Alex@MacBook-Pro-3.local>
2017-03-14 14:48:51 -0400

    shows duplicates and their amounts per week

--- app/models/week.rb
+++ app/models/week.rb
@@ -10,18 +10,22 @@ class Week < ActiveRecord::Base
     recipes.map(&:ingredients).flatten
   end
 
-  # def duplicate_ingredients
-  #   dupes = ingredients.find_all { |ingredient| ingredients.count(ingredient) > 1}
-  #
-  #   b = Hash.new(0)
-  #
-  #   dupes.each do |dupe|
-  #     b[dupe] += 1
-  #   end
-  #
-  #   b.each do |k, v|
-  #     puts "#{k} appears #{v} times"
-  #   end
-  # end
+  def duplicate_ingredients
+    ingredients.find_all { |ingredient| ingredients.count(ingredient) > 1}
+  end
+
+  def display_duplicates
+    b = Hash.new(0)
+
+    duplicate_ingredients.each do |dupe|
+      b[dupe] += 1
+    end
+
+    b
+
+    # b.each do |ingredient, v|
+    #   ingredient.name
+    # end
+  end
 
 end
